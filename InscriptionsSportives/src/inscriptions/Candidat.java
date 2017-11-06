85906c7e5e8e8eb01928a2dd727fb9f7f5b0b0a1
glimmois <glimmois@A35-16.MDNPEDAGO.local>
2017-11-06 16:18:46 +0100

    TEST2

--- InscriptionsSportives/src/inscriptions/Candidat.java
+++ InscriptionsSportives/src/inscriptions/Candidat.java
@@ -16,7 +16,7 @@ public abstract class Candidat implements Comparable<Candidat>, Serializable
 	private Inscriptions inscriptions;
 	private String nom;
 	private Set<Competition> competitions;
-	// SAM ERRRR
+	// SAM ERRRRRRR
 	Candidat(Inscriptions inscriptions, String nom)
 	{
 		this.inscriptions = inscriptions;	
